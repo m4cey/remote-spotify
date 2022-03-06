85f8bd14d2fb5f486eb4454cbdb12266abce857c
m4cey <h4mmer7ime@gmail.com>
2022-03-06 20:22:35 +0100

    idk

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fastify.get('/guide', function (req, reply) {
 
 const startServer = async () => {
   try {
-    await fastify.listen(8080)
+    await fastify.listen(8080, '0.0.0.0')
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
