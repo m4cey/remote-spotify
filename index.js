17d151b71e6bc7e16b5052cbec04c50fe10f760b
m4cey <h4mmer7ime@gmail.com>
2022-03-12 11:11:35 +0100

    nothing

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
 const { startServer } = require('./server.js');
 const { Client, Collection, Intents, MessageEmbed } = require('discord.js');
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require('dotenv').config();
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
