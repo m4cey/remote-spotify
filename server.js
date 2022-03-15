c3d875f9bdc8f86ddebeda89995499d72fb56ab6
m4cey <h4mmer7ime@gmail.com>
2022-03-15 13:49:00 +0100

    testing glitch

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ fastify.get('/guide', function (req, reply) {
 
 //update glitch with github webhooks
 fastify.post('/git', function (req, reply) {
+  logger.info('/git post event');
   if (env.process.ENV != 'glitch') return;
   let hmac = crypto.createHmac('sha1', process.env.GIT_SECRET);
   let sig = `sha1=${hmac.update(JSON.stringify(req.body)).digest('hex')}`;
