50ea3f46b4ab9cae4940a3e2e99a24a65e3a76f0
m4cey <h4mmer7ime@gmail.com>
2022-03-28 21:03:12 +0000

    idkk

--- commands/remote.js
+++ commands/remote.js
@@ -1,28 +1,26 @@
-const logger = require('../logger.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const methods = require('../methods.js');
+const logger = require("../logger.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const methods = require("../methods.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-	.setName('remote')
-	.setDescription('Start a party and control playback.'),
+  data: new SlashCommandBuilder()
+    .setName("remote")
+    .setDescription("Start a party and control playback."),
 
-	async execute(interaction) {
-		try {
-			logger.debug(`interaction ${interaction.id} beggining deferral`);
-			await interaction.deferReply();
-			logger.debug(`interaction ${interaction.id} has been deferred`);
-			const data = await methods.getUserData(interaction);
-			if (data?.length)
-      	data[0].queue = await methods.getQueue(data[0], 10);
-			const message = await methods.remoteMessage(data);
-			const lastMessage = methods.getLastMessage();
-			if (lastMessage)
-				lastMessage.edit(methods.blankMessage());
-			methods.setLastMessage(await interaction.editReply(message));
-		} catch (error) {
-			logger.error(error);
-			await interaction.reply(methods.failedMessage());
-		}
-	}
+  async execute(interaction) {
+    try {
+      logger.debug(`interaction ${interaction.id} beggining deferral`);
+      await interaction.deferReply();
+      logger.debug(`interaction ${interaction.id} has been deferred`);
+      const data = await methods.getUserData(interaction);
+      if (data?.length) data[0].queue = await methods.getQueue(data[0], 10);
+      const message = await methods.remoteMessage(data);
+      const lastMessage = methods.getLastMessage();
+      if (lastMessage) lastMessage.edit(methods.blankMessage());
+      methods.setLastMessage(await interaction.editReply(message));
+    } catch (error) {
+      logger.error(error);
+      await interaction.editReply(methods.failedMessage());
+    }
+  },
 };
--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-require('dotenv').config();
-const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
+require("dotenv").config();
+const fs = require("node:fs");
+const { Client, Collection, Intents } = require("discord.js");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 // Dynamic command and event handling
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, async (...args) => await event.execute(...args));
-	} else {
-		client.on(event.name, async (...args) => await event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, async (...args) => await event.execute(...args));
+  } else {
+    client.on(event.name, async (...args) => await event.execute(...args));
+  }
 }
 
 // Login to Discord with your client's token
