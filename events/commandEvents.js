4d39e164f13779d5759b015fd53efb634e553615
m4cey <h4mmer7ime@gmail.com>
2022-03-29 11:49:26 +0000

    have mercy

--- events/commandEvents.js
+++ events/commandEvents.js
@@ -1,18 +1,23 @@
-const logger = require('../logger.js');
+const logger = require("../logger.js");
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		if (!interaction.isCommand())	return;
-		logger.debug(`${interaction.user.tag} in #${interaction.channel.name} triggered a command.`);
-		const command = interaction.client.commands.get(interaction.commandName);
+  name: "interactionCreate",
+  async execute(interaction) {
+    if (!interaction.isCommand()) return;
+    logger.debug(
+      `${interaction.user.tag} in #${interaction.channel.name} triggered a command.`
+    );
+    const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) return;
+    if (!command) return;
 
-		try {
-			command.execute(interaction);
-		} catch (error) {
-			logger.error(error)
-			interaction.reply({ content: 'no work, sorry :(', ephemeral: true });
-		}
-	},
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      logger.error(error);
+      await interaction.reply({
+        content: "no work, sorry :(",
+        ephemeral: true,
+      });
+    }
+  },
 };
