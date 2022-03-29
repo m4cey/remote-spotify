cd953d146508863fba079ed5fb6beaab7515f51c
m4cey <h4mmer7ime@gmail.com>
2022-03-29 16:56:18 +0000

    I'm so done

--- commands/remote.js
+++ commands/remote.js
@@ -10,22 +10,15 @@ module.exports = {
   async execute(interaction) {
     try {
       await interaction.deferReply();
-      await interaction.editReply("WORK");
-      return;
       let data;
       if (methods.getLeaderId()) {
         data = await methods.getUserData(interaction);
-        logger.debug("userData recieved");
         if (data?.length) data[0].queue = await methods.getQueue(data[0], 10);
       }
       const message = await methods.remoteMessage(data);
-      logger.debug("remoteMessage recieved");
-      logger.debug(message);
       const lastMessage = methods.getLastMessage();
       if (lastMessage) lastMessage.edit(methods.blankMessage());
-      logger.debug(interaction);
       const newMessage = await interaction.editReply(message);
-      logger.debug("message sent?");
       methods.setLastMessage(newMessage);
     } catch (error) {
       logger.error(error);
